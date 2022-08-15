afe894851f1511cb680f6a389dd32ac2ae5c48a3
Siddharth Mantri <siddharthmantri1@gmail.com>
2022-08-15 17:16:46 +0200

    refactor bad guess and tests

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -43,26 +43,8 @@ defmodule Hangman.Impl.Game do
     |> return_with_tally()
   end
 
-  # def make_move(game = %{ game_state: :lost }, _) do #Pattern match on lost game state and ignore guess
-  #     { game, tally(game) }
-  # end
+   ####################################################
 
-  ####################################################
-  # private function defp
-  defp tally(game) do
-    %{
-      turns_left: game.turns_left,
-      game_state: game.game_state,
-      letters: [],
-      used: game.used |> MapSet.to_list() |> Enum.sort()
-    }
-  end
-
-  defp return_with_tally(game)  do
-    {game, tally(game)}
-  end  
-
-  ####################################################
   defp accept_guess(game, _guess, _already_used = true) do
     %{
       game | game_state: :already_used # remember Head and Tail in enums
@@ -77,21 +59,44 @@ defmodule Hangman.Impl.Game do
   end
 
   ####################################################
-  
+
   defp score_guess(game, _good_guess=true) do
     new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
     %{ game | game_state: new_state }
   end
 
   defp score_guess(game = %{ turns_left: 1 }, _bad_guess) do
-    %{ game | game_state: :lost }
+    %{ game | game_state: :lost, turns_left: 0 }
   end
 
   defp score_guess(game, _bad_guess) do
     %{ game | game_state: :bad_guess, turns_left: game.turns_left - 1 }
   end
 
+  ####################################################
+  # private function defp
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: reveal_guessed_letters(game),
+      used: game.used |> MapSet.to_list |> Enum.sort
+    }
+  end
+
+  defp return_with_tally(game)  do
+    {game, tally(game)}
+  end  
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
   defp maybe_won(true), do: :won
   defp maybe_won(_), do: :good_guess
+
+  defp maybe_reveal(true, letter),  do: letter
+  defp maybe_reveal(_,    _letter), do: "_"
 
 end
